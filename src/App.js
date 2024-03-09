2ab4fb37d948ea1f45751ee7b2f44423a316c524
1707akash <1707akash@gmail.com>
2024-03-09 06:43:08 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
 import Cards from './Components/Cards';
+import movies from './Movies.json';
 
 function App() {
   return (
     <>
-    <Cards></Cards>
+    {
+      movies.map((movies, index)=>{
+        return(
+          <Cards title={movies.title} 
+          year={movies.year} 
+          director={movies.director} 
+          duration = {movies.duration}
+          genre = {movies.genre} 
+          synopsis = {movies.synopsis}
+          wallpaper = {movies.wallpaper_image}
+          thumbnail = {movies.thumbnail_image}
+
+
+          />
+        )
+      })
+    }
     </>
   );
 }
